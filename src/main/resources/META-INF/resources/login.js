c2d60c4701e1a81e0e4e45f11dc64ca198ac2c51
mxl17 <mert.takil@edu.tbz.ch>
2021-09-29 16:36:20 +0200

    login Funktionalität erstellt

---
+++ src/main/resources/META-INF/resources/login.js
@@ -0,0 +1,19 @@
+const URL = 'http://localhost:8080';
+
+function login() {
+    let username = document.getElementById("uname").value;
+    let password = document.getElementById("psw").value;
+
+    fetch(`${URL}/auth/login`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: '{"password": "' + password + '", "username": "' + username + '"}'
+    }).then((result) => {
+        result.text().then((result) => {
+            console.log(result);
+            localStorage.setItem("token", result);
+        });
+    });
+}
