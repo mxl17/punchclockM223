e3cdcbc31e94366ced2e7a6c078671dda2e5e87c
mxl17 <mert.takil@edu.tbz.ch>
2021-09-29 20:07:23 +0200

    CategoryController erstellt

---
+++ src/main/java/ch/zli/m223/punchclock/controller/CategoryController.java
@@ -0,0 +1,56 @@
+package ch.zli.m223.punchclock.controller;
+
+import ch.zli.m223.punchclock.domain.Category;
+import ch.zli.m223.punchclock.service.CategoryService;
+import io.quarkus.security.Authenticated;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+@Path("/categories")
+@Authenticated
+@Tag(name = "Categories", description = "Handling of categories")
+public class CategoryController {
+    @Inject
+    CategoryService categoryService;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(summary = "List all Categories", description = "")
+    public List<Category> list() {
+        return categoryService.findAll();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/{id}")
+    public Category getCategory(@PathParam("id") Long id) {
+        return categoryService.findCategoryByID(id);
+    }
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Operation(summary = "Add a new Category", description = "The newly created entry is returned. The id may not be passed.")
+    public Category add(Category category) {
+        return categoryService.createCategory(category);
+    }
+
+    @Path("/{id}")
+    @DELETE
+    @Operation(summary = "Deletes a Category", description = "")
+    public void delete(@PathParam("id") Long id) {
+        categoryService.deleteCategory(id);
+    }
+
+    @PUT
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Category updateCategory(Category category) {
+        return categoryService.update(category);
+    }
+}
