e49b5a0dfca78dc8bfd464891b7da1f5428b82d3
mxl17 <mert.takil@edu.tbz.ch>
2021-09-30 15:21:35 +0200

    import angepasst

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,3 @@
-INSERT INTO entry (checkin, checkout, category_id, user_id, workspace_id) VALUES ('2021-09-22T13:21:58.737', '2021-09-22T13:22:58.757', '1', '1', '2');
 INSERT INTO category (title) VALUES ('Interne Ausbildungen');
 INSERT INTO category (title) VALUES ('Projekt');
 INSERT INTO category (title) VALUES ('Externe Ausbildungen');
