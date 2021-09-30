4e73223a0792d930f82330f93b822d66c1a773d2
mxl17 <mert.takil@edu.tbz.ch>
2021-09-30 10:16:34 +0200

    Insert Queries erweitert und angepasst

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,7 @@
-INSERT INTO category (title) VALUES ('Admin');
+INSERT INTO category (title) VALUES ('Interne Ausbildungen');
 INSERT INTO category (title) VALUES ('Projekt');
-INSERT INTO category (title) VALUES ('IT-Support');
+INSERT INTO category (title) VALUES ('Externe Ausbildungen');
+INSERT INTO workspace (title) VALUES ('Büro');
+INSERT INTO workspace (title) VALUES ('Home Office');
+INSERT INTO workspace (title) VALUES ('Extern');
 INSERT INTO User (PASSWORD, USERNAME) VALUES ('test', 'testUser');
