ad31a54c6c2a2133f5444c8145144cfe7cda232d
mxl17 <mert.takil@edu.tbz.ch>
2021-09-30 09:01:53 +0200

    WorkspaceService implementiert

--- src/main/java/ch/zli/m223/punchclock/service/WorkspaceService.java
+++ src/main/java/ch/zli/m223/punchclock/service/WorkspaceService.java
@@ -1,4 +1,48 @@
 package ch.zli.m223.punchclock.service;
 
+import ch.zli.m223.punchclock.domain.Workspace;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@RequestScoped
 public class WorkspaceService {
+    @Inject
+    private EntityManager entityManager;
+
+    public WorkspaceService() {
+
+    }
+
+    @Transactional
+    public Workspace createWorkspace(Workspace workspace) {
+        entityManager.persist(workspace);
+        return workspace;
+    }
+
+    @Transactional
+    public void deleteWorkspace(Long id) {
+        entityManager.remove(entityManager.find(Workspace.class, id));
+    }
+
+    @Transactional
+    public Workspace findWorkspaceByID(Long id) {
+        Workspace findWorkspace = entityManager.find(Workspace.class, id);
+        return findWorkspace;
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<Workspace> findAll() {
+        var query = entityManager.createQuery("FROM Workspace");
+        return query.getResultList();
+    }
+
+    @Transactional
+    public Workspace update(Workspace workspace) {
+        entityManager.merge(workspace);
+        return workspace;
+    }
 }
